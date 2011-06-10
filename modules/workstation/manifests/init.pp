f25fd9702b7643b3fe1fec87aa287098244d2395
Jordan Sissel <jls@semicomplete.com>
2011-06-10 16:24:37 -0700

    - skype sucks

--- modules/workstation/manifests/init.pp
+++ modules/workstation/manifests/init.pp
@@ -1,4 +1,5 @@
 class workstation {
+
   package {
     [ "zsh", "screen", "tmux", "vim-enhanced" ]: ensure => latest;
     "wget": ensure => latest;
@@ -15,10 +16,25 @@ class workstation {
     "glib2-devel": ensure => latest;
     "cairo-devel": ensure => latest;
     "xorg-x11-apps": ensure => latest;
+    "strace": ensure => latest;
 
     # X11
     "libX11-devel": ensure => latest;
     "libXinerama-devel": ensure => latest;
     "libXtst-devel": ensure => latest;
+
+    # skype deps
+    "alsa-lib.i686": ensure => latest;
+    "glibc.i686": ensure => latest;
+    "libgcc.i686": ensure => latest;
+    "libstdc++.i686": ensure => latest;
+    "libX11.i686": ensure => latest;
+    "libXau.i686": ensure => latest;
+    "libxcb.i686": ensure => latest;
+    "libXext.i686": ensure => latest;
+    "libXScrnSaver.i686": ensure => latest;
+    "libXv.i686": ensure => latest;
+    "qt-x11.i686": ensure => latest;
+
   }
 }
