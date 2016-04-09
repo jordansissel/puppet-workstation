0700fe4a9adebb6afc745228a0cebfc4b77b8b57
Jordan Sissel <jls@semicomplete.com>
2016-04-08 17:36:55 -0700

    wheel group

--- modules/user/manifests/humans.pp
+++ modules/user/manifests/humans.pp
@@ -1,8 +1,13 @@
 class user::humans {
   include homedir
 
+  group {
+    "wheel": ensure => present;
+  }
+
   user {
     "jls":
+      require => Group["wheel"],
       ensure => present,
       groups => ["wheel"],
       shell => "/bin/zsh";
