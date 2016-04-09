4d584bab40ac2251a53a062e9e28a4f071fcc195
Jordan Sissel <jls@semicomplete.com>
2016-04-08 17:37:14 -0700

    Split packages by vendor if needed

--- modules/workstation/manifests/init.pp
+++ modules/workstation/manifests/init.pp
@@ -1,34 +1,45 @@
 class workstation {
   package {
-    [ "zsh", "screen", "tmux", "vim-enhanced" ]: ensure => latest;
+    [ "zsh", "screen", "tmux"]: ensure => latest;
     "wget": ensure => latest;
     "curl": ensure => latest;
 
     # dev tools
     "git": ensure => latest;
     "gcc": ensure => latest;
-    "gcc-c++": ensure => latest;
-    "libtool-ltdl": ensure => latest;
     "libtool": ensure => latest;
-    "libtool-ltdl-devel": ensure => latest;
-    "ctags": ensure => latest; "cscope": ensure => latest;
-    [ "lua", "lua-devel" ]: ensure => latest;
-
-    "readline": ensure => latest;
-    "readline-devel": ensure => latest;
-    "openssl-devel": ensure => latest;
-    "glib-devel": ensure => latest;
-    "glib2-devel": ensure => latest;
-    "cairo-devel": ensure => latest;
+    "ctags": ensure => latest;
+    "cscope": ensure => latest;
     "strace": ensure => latest;
-
+    "tcpdump": ensure => latest;
     "valgrind": ensure => latest;
     "pwgen": ensure => latest;
     "automake": ensure => latest;
     "nmap": ensure => latest;
+  }
 
-    "libxml2-devel": ensure => latest;
-    "libxslt-devel": ensure => latest;
-
+  case $os["distro"]["id"] {
+    'Fedora', 'RedHat', 'CentOS': {
+      package {
+        "gcc-c++": ensure => latest;
+        "libtool-ltdl": ensure => latest;
+        "libtool-ltdl-devel": ensure => latest;
+        [ "lua", "lua-devel" ]: ensure => latest;
+        "readline": ensure => latest;
+        "readline-devel": ensure => latest;
+        "openssl-devel": ensure => latest;
+        "vim-enhanced": ensure => latest;
+      }
+    }
+    'Debian', 'Ubuntu': {
+      package {
+        "g++": ensure => latest;
+        "libltdl-dev": ensure => latest;
+        "liblua5.1-dev": ensure => latest;
+        "libreadline6": ensure => latest;
+        "libssl-dev": ensure => latest;
+        "openjdk-8-jdk": ensure => latest;
+      }
+    }
   }
 }
