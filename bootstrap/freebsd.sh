d075f43176369ed9f198d8f45562b3836185dd54
Jordan Sissel <jls@semicomplete.com>
2016-05-14 20:07:41 -0700

    Update freebsd.sh

--- bootstrap/freebsd.sh
+++ bootstrap/freebsd.sh
@@ -4,7 +4,7 @@ mkdir ~/.ssh
 # freebsd 10.3's fetch doesn't trust github's cert. uugh.
 fetch --no-verify-peer -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/jordansissel/dotfiles/master/.ssh/authorized_keys
 
-pkg install puppet
+pkg install puppet4
 
 
 
