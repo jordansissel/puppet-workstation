52e67b3be313299b959a9ddd587b362aa38552d5
Jordan Sissel <jls@semicomplete.com>
2016-05-18 09:54:25 -0700

    Update suse.sh

--- bootstrap/suse.sh
+++ bootstrap/suse.sh
@@ -3,3 +3,5 @@
 zypper install rubygem-puppet wget
 mkdir ~/.ssh
 wget -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/jordansissel/dotfiles/master/.ssh/authorized_keys
+systemctl enable sshd
+systemctl start sshd
